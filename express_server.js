f6f517cb0062ce229e9a4ce8c6ab8b869e336097
Vivian Cheung <hi@viviancheung.ca>
2021-06-07 19:26:59 +0000

    added more dependencies

--- express_server.js
+++ express_server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
-
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,4 +17,8 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
