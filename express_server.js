210e0ed16b0816f5bc1433de9223fb0c63884ead
Vivian Cheung <hi@viviancheung.ca>
2021-06-10 00:08:55 +0000

    added register page, 404 messages for edge cases

--- express_server.js
+++ express_server.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
+// database of URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({extented: true}));
-app.use(cookieParser());
+// database of users
+const users = {};
 
+// turns on server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -21,30 +25,39 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// page for users to create new tiny link
+// must be above route for /urls/:shortURL, 
+// otherwise express will believe that urls_new is a new route parameter
 app.get("/urls/new", (req, res) => {
+  const user = users[req.cookies["user_id"]];
   const templateVars = {
-    username: req.cookies["username"]
+    user,
   };
   res.render("urls_new", templateVars);
 });
 
+// shows all URLs in database, basically home page if logged in
 app.get("/urls", (req, res) => {
+  const user = users[req.cookies["user_id"]];
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user,
   };
   res.render("urls_index", templateVars);
 });
 
+// shows edit page for :shortURL
 app.get("/urls/:shortURL", (req, res) => {
+  const user = users[req.cookies["user_id"]];
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+    user,
   };
   res.render("urls_show", templateVars);
 });
 
+// redirects from tiny link to long URL associated with :shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -57,23 +70,34 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// helper function: ID generator
 const generateRandomString = function() {
   //let key = Math.random().toString(36).substr(2, 8);
   let key = "";
   let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-
-  
   for (let i = 0; i < 6; i++) {
     key += char[Math.floor(Math.random() * char.length)];
   }
   return key;
 };
 
+// helper function: checks if email given already exists in database "users"
+const isEmailDuplicate = function(email) {
+  for (let userID in users) {
+    if (email === users[userID][email]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// deletes :shortURL entry from database
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls`);
 });
 
+// 
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
@@ -81,23 +105,61 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
+  const user = users[req.cookies["user_id"]];
   const templateVars = {
     shortURL,
     longURL,
-    username: req.cookies["username"]
+    user
   };
   res.render("urls_show", templateVars);
 });
 
+// shows login page
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
-  res.redirect(`/urls`);
+  res.redirect("/urls");
 });
 
+// logs user out
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect(`/urls`);
+  res.clearCookie('user_id');
+  res.redirect("/urls");
 });
+
+
+// shows registration page
+app.get("/register", (req, res) => {
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {
+    user,
+  };
+  res.render("register", templateVars);
+});
+
+// allows user to input data to register page
+// if email or password fields are empty, returns 404
+// if email is duplicate, returns 404
+// else, creates new account
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(400).send("Whoops! Please try again.");
+  }
+  if (!isEmailDuplicate(email)) {
+    return res.status(400).send("Whoops! Please try again.");
+  }
+ 
+  users[userID] = { "id": userID, "email": email, "password": password };
+  res.cookie('user_id', userID);
+  res.redirect("/urls");
+});
+
+
+
